a70bc1865c37659292325592dac18cb6166cb879
vvakame <vvakame+dev@gmail.com>
2017-02-16 13:55:29 +0900

    add nullable type support function

--- value.go
+++ value.go
@@ -1,7 +1,35 @@
 package gopan
 
-import "cloud.google.com/go/spanner"
+import (
+	"time"
 
-func String(str string) spanner.NullString {
-	return spanner.NullString{Valid: true, StringVal: str}
+	"cloud.google.com/go/civil"
+	"cloud.google.com/go/spanner"
+)
+
+func String(v string) spanner.NullString {
+	return spanner.NullString{Valid: true, StringVal: v}
+}
+
+func Time(v time.Time) spanner.NullTime {
+	if v.IsZero() {
+		return spanner.NullTime{}
+	}
+	return spanner.NullTime{Valid: true, Time: v}
+}
+
+func Float64(v float64) spanner.NullFloat64 {
+	return spanner.NullFloat64{Valid: true, Float64: v}
+}
+
+func Int64(v int64) spanner.NullInt64 {
+	return spanner.NullInt64{Valid: true, Int64: v}
+}
+
+func Date(v civil.Date) spanner.NullDate {
+	return spanner.NullDate{Valid: true, Date: v}
+}
+
+func Bool(v bool) spanner.NullBool {
+	return spanner.NullBool{Valid: true, Bool: v}
 }
